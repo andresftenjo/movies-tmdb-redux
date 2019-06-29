0797361a5ef82c5d1f7165e434c0ab4cbcf84ac7
Andres Tenjo <useda.theli@gmail.com>
2019-06-29 15:34:04 -0500

    fixing null movie player call issue

--- src/components/movie/MovieDetailPage.js
+++ src/components/movie/MovieDetailPage.js
@@ -6,7 +6,12 @@ const MovieDetailPage = ({ movievideos, moviedetails }) => (
       <div className="starRating"><p>{moviedetails.vote_average}</p></div><h4>{moviedetails.title}</h4>
       <p className="description">{moviedetails.overview}</p>
       <div className="videoContainer">
-      <iframe frameBorder="0" scrolling="no" width="100%" height="400px" type="text/html" src={`https://www.youtube.com/embed/${movievideos.key}?autoplay=1&fs=0&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0`}></iframe>
+      {movievideos.key ? (
+          <iframe frameBorder="0" scrolling="no" width="100%" height="400px" type="text/html" src={`https://www.youtube.com/embed/${movievideos.key}?autoplay=1&fs=0&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=0&start=0&end=0`}></iframe>
+          ) : (
+            <> 
+            </>
+          )}
       </div>
       <ul className="list-group">
         {moviedetails.genres ? moviedetails.genres.map(genre => {
